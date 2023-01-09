3c1152129cf272838eb3c40f6837d0bf44302b4d
Nuno Neto <nunoneto2015@gmail.com>
2023-01-09 19:53:25 +0000

    created getMovieInfo

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import { useState, useEffect } from "react";
 import './App.css';
 import api from "./api"; 
 import MovieRow from "./components/MovieRow/MovieRow";
+import FeaturedMovie from "./components/FeaturedMovie/FeaturedMovie";
 
 
 function App() {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState([null]);
 
   useEffect(() => {
     const loadAll = async () => {
       const list = await api.getHomeList();
       setMovieList(list);
+
+      let originals = list.filter(movie => movie.slug === "originals");
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await api.getMovieInfo(chosen.id, "tv");
+      setFeaturedData(chosenInfo);
+
     }
 
     loadAll();
@@ -19,6 +28,9 @@ function App() {
 
   return (
     <div className="page">
+      {featuredData && 
+        <FeaturedMovie item={featuredData}/> 
+      }
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
---
+++ src/components/FeaturedMovie/FeaturedMovie.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./FeaturedMovie.css";
+
+const FeaturedMovie = () => {
+  return (
+    <div>FeaturedMovie</div>
+  )
+}
+
+export default FeaturedMovie;
